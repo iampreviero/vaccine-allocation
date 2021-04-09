0098e6ed1d6da1b2b053f85b17dd9a12117d9db9
Michael Li <shaznl1@hotmail.com>
2021-04-08 21:57:50 -0400

    Update True Vaccine

--- src/pipeline/post_processing.py
+++ src/pipeline/post_processing.py
@@ -5,13 +5,14 @@ Created on Wed Feb  3 21:16:06 2021
 @author: Michael
 """
 
-with open("../data/outputs/optimized-solution--20210208-111721-0-0.pickle","rb") as f:
+states = sorted(county_pop_df.state.unique())
+with open("../data/outputs/optimized-solution--20210209-200037-0-0.pickle","rb") as f:
     solution = pickle.load(f)
 vac_dist_states = np.zeros(solution.vaccinated.shape[0])
 for j in range(vac_dist_states.shape[0]):
     vac_dist_states[j] = sum(solution.vaccine_distribution[i,:].sum() for i in allocation_params["state_to_cities"][j])
 total_deaths = (solution.deceased + solution.hospitalized_dying + solution.quarantined_dying)[:, :, -1].sum(axis=1)
-pd.DataFrame(data={"state": states, "num_locations": solution.locations,"vac_distributions": vac_dist_states, "total_deaths": total_deaths}).to_csv("optimized_no_limits.csv")
+pd.DataFrame(data={"state": states, "num_locations": solution.locations,"vac_distributions": vac_dist_states, "total_deaths": total_deaths}).to_csv("optimized_all_ok.csv")
 
 
 
